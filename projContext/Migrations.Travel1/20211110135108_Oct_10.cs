27d0473a8c01e57eb86aca61c88a6bd3a0c9ab49
PRABHAKAR KUMAR SINGH <prabhakarks90@hotmail.com>
2021-11-10 19:22:17 +0530

    test

---
+++ projContext/Migrations.Travel1/20211110135108_Oct_10.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace projContext.Migrations.Travel1
+{
+    public partial class Oct_10 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
