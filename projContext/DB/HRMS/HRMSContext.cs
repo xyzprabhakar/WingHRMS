1a8259fb11b7bba3c1e12633de96cc23184f9214
PRABHAKAR KUMAR SINGH <prabhakarks90@hotmail.com>
2022-01-08 17:03:58 +0530

    hrms

---
+++ projContext/DB/HRMS/HRMSContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projContext.DB.HRMS
+{
+    public class HRMSContext : DbContext
+    {
+        //add-migration -s projApi 31_Oct_20211 -Context projContext.DB.HRMS.HRMSContext
+        //update-database -s projApi -Context projContext.DB.HRMS.HRMSContext
+        public HRMSContext(DbContextOptions<HRMSContext> options) : base(options)
+        {
+
+        }
+        public DbSet<tblHolidayMaster> tblHolidayMaster { get; set; }
+        public DbSet<tblDepartment> tblDepartment { get; set; }
+        public DbSet<tblDepartWorkingRole> tblDepartWorkingRole { get; set; }
+        public DbSet<tblGrade> tblGrade { get; set; }
+        public DbSet<tblDesignation> tblDesignation { get; set; }
+        public DbSet<tblReligionMaster> tblReligionMaster { get; set; }
+        
+    }
+}
